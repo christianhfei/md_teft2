5738cf525b4f9aede656fae9ff51cd041dc58c48
christianhfei <Christian.Heyd@fei.local>
2016-11-03 15:51:09 -0400

    Added starting files

---
+++ areas/home/home.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+    var module = angular.module("macwisWebApp");
+
+    var controller = function ($scope) {
+        var model = this;
+        model.title = "MyLTSS - Home";
+    };
+
+    module.component("home", {
+        templateUrl: "areas/home/home.html",
+        controllerAs: "model",
+        controller: ["$scope", controller]
+
+    });
+}())
