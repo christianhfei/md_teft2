46894ca2a41bfb0b336740adac32a76a3a0d7114
Luis Najera <luis.najera@feisystems.com>
2016-11-13 22:54:56 -0500

    Initial check in of the TEFT services module.

--- areas/services/services.js
+++ areas/services/services.js
@@ -5,6 +5,92 @@
     var controller = function ($scope) {
         var model = this;
         model.title = "MyLTSS - Services";
+
+        $scope.init = function() {
+            setTimeout(function(){
+                $('#servicesCalendar').fullCalendar({
+                    "defaultDate" : "2016-01-01",
+                    "customButtons": {
+                        "showFlaggedServices": {
+                            "text": 'Show Flagged Services',
+                            click: function() {
+                                alert('clicked the custom button!');
+                            }
+                        },
+                        "advancedSearch": {
+                            "text": 'Advanced Search',
+                            click: function() {
+                                alert('clicked the custom button!');
+                            }
+                        }
+                    },
+                    "header" : {
+                        "left":   'prev title next',
+                        "center": 'showFlaggedServices advancedSearch',
+                        "right":  'today  month,basicWeek,listDay'
+                    },
+                    "views": {
+                        "listDay": {
+                            type: 'list',
+                            duration: { days: 1 },
+                            buttonText: 'Day'
+                        }
+                    },
+                    "viewRender": function(view, element) {
+
+                        if (view.name == "listDay") {
+                            $(".fc-view-container").addClass("col-xs-6");
+                            $("#serviceDetails").show();
+                        } else {
+                            $(".fc-view-container").removeClass("col-xs-6");
+                            $("#serviceDetails").hide();
+                        }
+                    },
+                    events: [
+                        {
+                            id: '1',
+                            title: 'Shared Attendant',
+                            start: '2016-01-15'
+                        },
+                        {
+                            id: '2',
+                            title: 'Shared Attendant',
+                            start: '2016-01-13'
+                        },
+                        {
+                            id: '3',
+                            title: 'Shared Attendant',
+                            start: '2016-01-11'
+                        },
+                        {
+                            id: '4',
+                            title: 'Personal Assistance',
+                            start: '2016-01-09'
+                        },
+                        {
+                            id: '5',
+                            title: 'Shared Attendant',
+                            start: '2016-01-06'
+                        },
+                        {
+                            id: '6',
+                            title: 'Personal Assistance',
+                            start: '2016-01-04'
+                        }, {
+                            id: '7',
+                            title: 'Shared Attendant',
+                            start: '2016-01-03'
+                        }, {
+                            id: '8',
+                            title: 'Shared Attendant',
+                            start: '2016-01-01'
+                        }
+                    ]
+                });
+            },400)
+        }
+
+        $scope.init();
     };
 
     module.component("myServices", {
