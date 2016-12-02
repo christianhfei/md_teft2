15d6bd8331ad0fd7b97b4c922f5d323ea44bb116
Luis Najera <luis.najera@feisystems.com>
2016-12-02 13:27:48 -0500

    Added functionality to hide the footer from in any view. Applied the feature in the registration page.

--- areas/clientregistration/register.js
+++ areas/clientregistration/register.js
@@ -2,15 +2,22 @@
     "use strict";
     var module = angular.module("macwisWebApp");
 
-    var controller = function ($scope) {
+    var controller = function ($rootScope, $scope, $location) {
         var model = this;
         model.title = "MyLTSS - Representative Registration";
+
+        $rootScope.$emit("hideFooter", true);
+
+        //Show footer again after leaving this page.
+        $rootScope.$on("$locationChangeStart", function(){
+            $rootScope.$emit("hideFooter", false);
+        });
     };
 
     module.component("clientRegister", {
         templateUrl: "areas/clientregistration/register.html",
         controllerAs: "model",
-        controller: ["$scope", controller]
+        controller: ["$rootScope", "$scope", "$location", controller]
 
     });
 }())
